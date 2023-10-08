760d41257aa586dc8d321118bef7b9f08e5f114a
omerdemirok <omerdemirok@icloud.com>
2023-10-08 10:49:16 +0100

    add unit test for delete db

--- xata/databases_client_test.go
+++ xata/databases_client_test.go
@@ -58,7 +58,7 @@ func Test_databaseCli_Create(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			testSrv := testService(t, http.MethodPut, "", tt.statusCode, tt.apiErr != nil, tt.want)
+			testSrv := testService(t, http.MethodPut, "/workspaces", tt.statusCode, tt.apiErr != nil, tt.want)
 
 			cli, err := xata.NewDatabasesClient(xata.WithBaseURL(testSrv.URL), xata.WithAPIKey("test-key"))
 			if err != nil {
@@ -66,6 +66,65 @@ func Test_databaseCli_Create(t *testing.T) {
 			}
 
 			got, err := cli.Create(context.TODO(), tt.request)
+
+			if tt.apiErr != nil {
+				errAPI := tt.apiErr.Unwrap()
+				if errAPI == nil {
+					t.Fatal("expected error but got nil")
+				}
+				assert.ErrorAs(err, &errAPI)
+				assert.Equal(err.Error(), tt.apiErr.Error())
+				assert.Nil(got)
+			} else {
+				assert.Equal(tt.want, got)
+				assert.NoError(err)
+			}
+		})
+	}
+}
+
+func Test_databaseCli_Delete(t *testing.T) {
+	assert := assert.New(t)
+
+	type tc struct {
+		name       string
+		request    xata.DeleteDatabaseRequest
+		want       *xatagen.DeleteDatabaseResponse
+		statusCode int
+		apiErr     *xatagencore.APIError
+	}
+
+	tests := []tc{
+		{
+			name: "should delete db",
+			request: xata.DeleteDatabaseRequest{
+				DatabaseName: "db-name",
+			},
+			want: &xatagen.DeleteDatabaseResponse{
+				Status: xatagen.MigrationStatusCompleted,
+			},
+			statusCode: http.StatusOK,
+		},
+	}
+
+	for _, eTC := range errTestCasesCore {
+		tests = append(tests, tc{
+			name:       eTC.name,
+			statusCode: eTC.statusCode,
+			apiErr:     eTC.apiErr,
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testSrv := testService(t, http.MethodDelete, "/workspaces", tt.statusCode, tt.apiErr != nil, tt.want)
+
+			cli, err := xata.NewDatabasesClient(xata.WithBaseURL(testSrv.URL), xata.WithAPIKey("test-key"))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			got, err := cli.Delete(context.TODO(), tt.request)
 
 			if tt.apiErr != nil {
 				errAPI := tt.apiErr.Unwrap()
