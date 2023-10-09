9761534449f58600aace93c7cdeb866f7153eb8c
omerdemirok <omerdemirok@icloud.com>
2023-10-09 12:33:11 +0100

    add unit test for delete table

--- xata/table_client_test.go
+++ xata/table_client_test.go
@@ -87,3 +87,64 @@ func Test_tableClient_Create(t *testing.T) {
 		})
 	}
 }
+
+func Test_tableClient_Delete(t *testing.T) {
+	assert := assert.New(t)
+
+	type tc struct {
+		name       string
+		request    xata.TableRequest
+		want       *xatagenworkspace.DeleteTableResponse
+		statusCode int
+		apiErr     *xatagencore.APIError
+	}
+
+	tests := []tc{
+		{
+			name: "should delete a table",
+			request: xata.TableRequest{
+				DatabaseName: xata.String("db-name"),
+				TableName:    "table-name",
+			},
+			want:       &xatagenworkspace.DeleteTableResponse{Status: xatagenworkspace.MigrationStatusCompleted},
+			statusCode: http.StatusCreated,
+		},
+	}
+
+	for _, eTC := range errTestCasesWorkspace {
+		tests = append(tests, tc{
+			name: eTC.name,
+			request: xata.TableRequest{
+				DatabaseName: xata.String("db-name"),
+				TableName:    "table-name",
+			},
+			statusCode: eTC.statusCode,
+			apiErr:     eTC.apiErr,
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testSrv := testService(t, http.MethodDelete, "/db", tt.statusCode, tt.apiErr != nil, tt.want)
+
+			cli, err := xata.NewTableClient(xata.WithBaseURL(testSrv.URL), xata.WithAPIKey("test-key"))
+			assert.NoError(err)
+			assert.NotNil(cli)
+
+			got, err := cli.Delete(context.TODO(), tt.request)
+
+			if tt.apiErr != nil {
+				errAPI := tt.apiErr.Unwrap()
+				if errAPI == nil {
+					t.Fatal("expected error but got nil")
+				}
+				assert.ErrorAs(err, &errAPI)
+				assert.Equal(err.Error(), tt.apiErr.Error())
+				assert.Nil(got)
+			} else {
+				assert.NoError(err)
+				assert.Equal(tt.want, got)
+			}
+		})
+	}
+}
