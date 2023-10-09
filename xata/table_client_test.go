e0106322c704457e41757927ba045c8bbeee2115
omerdemirok <omerdemirok@icloud.com>
2023-10-09 12:40:44 +0100

    add unit test for adding column

--- xata/table_client_test.go
+++ xata/table_client_test.go
@@ -148,3 +148,86 @@ func Test_tableClient_Delete(t *testing.T) {
 		})
 	}
 }
+
+func Test_tableClient_AddColumn(t *testing.T) {
+	assert := assert.New(t)
+
+	type tc struct {
+		name       string
+		request    xata.AddColumnRequest
+		want       *xatagenworkspace.AddTableColumnResponse
+		statusCode int
+		apiErr     *xatagencore.APIError
+	}
+
+	tests := []tc{
+		{
+			name: "should create a table",
+			request: xata.AddColumnRequest{
+				TableRequest: xata.TableRequest{
+					DatabaseName: xata.String("db-name"),
+					TableName:    "table-name",
+				},
+				Column: &xata.Column{
+					Name:         "user-name",
+					Type:         xata.ColumnTypeString,
+					NotNull:      xata.Bool(true),
+					DefaultValue: xata.String("defaultValue"),
+					Unique:       xata.Bool(false),
+				},
+			},
+			want: &xatagenworkspace.AddTableColumnResponse{
+				MigrationId:       "mig-id",
+				ParentMigrationId: "parent-mig-id",
+				Status:            xatagenworkspace.MigrationStatusCompleted,
+			},
+			statusCode: http.StatusOK,
+		},
+	}
+
+	for _, eTC := range errTestCasesWorkspace {
+		tests = append(tests, tc{
+			name: eTC.name,
+			request: xata.AddColumnRequest{
+				TableRequest: xata.TableRequest{
+					DatabaseName: xata.String("db-name"),
+					TableName:    "table-name",
+				},
+				Column: &xata.Column{
+					Name:         "user-name",
+					Type:         xata.ColumnTypeString,
+					NotNull:      xata.Bool(true),
+					DefaultValue: xata.String("defaultValue"),
+					Unique:       xata.Bool(false),
+				},
+			},
+			statusCode: eTC.statusCode,
+			apiErr:     eTC.apiErr,
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testSrv := testService(t, http.MethodPost, "/db", tt.statusCode, tt.apiErr != nil, tt.want)
+
+			cli, err := xata.NewTableClient(xata.WithBaseURL(testSrv.URL), xata.WithAPIKey("test-key"))
+			assert.NoError(err)
+			assert.NotNil(cli)
+
+			got, err := cli.AddColumn(context.TODO(), tt.request)
+
+			if tt.apiErr != nil {
+				errAPI := tt.apiErr.Unwrap()
+				if errAPI == nil {
+					t.Fatal("expected error but got nil")
+				}
+				assert.ErrorAs(err, &errAPI)
+				assert.Equal(err.Error(), tt.apiErr.Error())
+				assert.Nil(got)
+			} else {
+				assert.NoError(err)
+				assert.Equal(tt.want, got)
+			}
+		})
+	}
+}
