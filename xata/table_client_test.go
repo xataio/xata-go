e2b3ed042bf18240c71350f5364720b143f14f7c
omerdemirok <omerdemirok@icloud.com>
2023-10-09 12:45:22 +0100

    add unit test for deleting column

--- xata/table_client_test.go
+++ xata/table_client_test.go
@@ -231,3 +231,74 @@ func Test_tableClient_AddColumn(t *testing.T) {
 		})
 	}
 }
+
+func Test_tableClient_DeleteColumn(t *testing.T) {
+	assert := assert.New(t)
+
+	type tc struct {
+		name       string
+		request    xata.DeleteColumnRequest
+		want       *xatagenworkspace.DeleteColumnResponse
+		statusCode int
+		apiErr     *xatagencore.APIError
+	}
+
+	tests := []tc{
+		{
+			name: "should create a table",
+			request: xata.DeleteColumnRequest{
+				TableRequest: xata.TableRequest{
+					DatabaseName: xata.String("db-name"),
+					TableName:    "table-name",
+				},
+				ColumnName: "col-name",
+			},
+			want: &xatagenworkspace.DeleteColumnResponse{
+				MigrationId:       "mig-id",
+				ParentMigrationId: "par-mig-id",
+				Status:            xatagenworkspace.MigrationStatusCompleted,
+			},
+			statusCode: http.StatusOK,
+		},
+	}
+
+	for _, eTC := range errTestCasesWorkspace {
+		tests = append(tests, tc{
+			name: eTC.name,
+			request: xata.DeleteColumnRequest{
+				TableRequest: xata.TableRequest{
+					DatabaseName: xata.String("db-name"),
+					TableName:    "table-name",
+				},
+				ColumnName: "col-name",
+			},
+			statusCode: eTC.statusCode,
+			apiErr:     eTC.apiErr,
+		})
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testSrv := testService(t, http.MethodDelete, "/db", tt.statusCode, tt.apiErr != nil, tt.want)
+
+			cli, err := xata.NewTableClient(xata.WithBaseURL(testSrv.URL), xata.WithAPIKey("test-key"))
+			assert.NoError(err)
+			assert.NotNil(cli)
+
+			got, err := cli.DeleteColumn(context.TODO(), tt.request)
+
+			if tt.apiErr != nil {
+				errAPI := tt.apiErr.Unwrap()
+				if errAPI == nil {
+					t.Fatal("expected error but got nil")
+				}
+				assert.ErrorAs(err, &errAPI)
+				assert.Equal(err.Error(), tt.apiErr.Error())
+				assert.Nil(got)
+			} else {
+				assert.NoError(err)
+				assert.Equal(tt.want, got)
+			}
+		})
+	}
+}
