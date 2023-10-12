35d3a3eaec84e87fb8bb46e77132a1f702403d12
omerdemirok <omerdemirok@icloud.com>
2023-10-12 22:45:07 +0100

    add get workspace method and integration test

--- internal/integration-tests/workspaces_client_test.go
+++ internal/integration-tests/workspaces_client_test.go
@@ -10,46 +10,37 @@ import (
 	"github.com/xataio/xata-go/xata"
 )
 
-func Test_workspacesClient_List(t *testing.T) {
-	apiKey, found := os.LookupEnv("XATA_API_KEY")
-	if !found {
-		t.Skipf("%s not found in env vars", "XATA_API_KEY")
-	}
-
-	t.Run("should list the workspaces for the given API key", func(t *testing.T) {
-		workspaceCli, err := xata.NewWorkspacesClient(xata.WithAPIKey(apiKey))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		resp, err := workspaceCli.List(context.Background())
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		assert.NoError(t, err)
-		assert.NotNil(t, resp)
-	})
-}
-
 func Test_workspacesClient_Create_Delete(t *testing.T) {
 	apiKey, found := os.LookupEnv("XATA_API_KEY")
 	if !found {
 		t.Skipf("%s not found in env vars", "XATA_API_KEY")
 	}
 
-	t.Run("should create and delete a workspace", func(t *testing.T) {
+	t.Run("should create, get, list and delete workspace", func(t *testing.T) {
 		workspaceCli, err := xata.NewWorkspacesClient(xata.WithAPIKey(apiKey))
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		ws, err := workspaceCli.Create(context.Background(), &xata.WorkspaceMeta{Name: "test-integration", Slug: xata.String("test_integration")})
+		ctx := context.Background()
+
+		ws, err := workspaceCli.Create(ctx, &xata.WorkspaceMeta{Name: "test-integration", Slug: xata.String("test_integration")})
 		if err != nil {
 			t.Fatal(err)
 		}
-
 		assert.NotNil(t, ws)
+
+		workspace, err := workspaceCli.GetWithWorkspaceID(ctx, ws.Id)
+		assert.Nil(t, err)
+		assert.Equal(t, ws.Id, workspace.Id)
+
+		resp, err := workspaceCli.List(context.Background())
+		assert.Nil(t, err)
+		var wsIDs []string
+		for _, ws := range resp.Workspaces {
+			wsIDs = append(wsIDs, ws.Id)
+		}
+		assert.Contains(t, wsIDs, workspace.Id)
 
 		err = workspaceCli.Delete(context.Background(), ws.Id)
 		assert.NoError(t, err)
--- xata/workspaces_client.go
+++ xata/workspaces_client.go
@@ -2,6 +2,7 @@ package xata
 
 import (
 	"context"
+	"log"
 
 	xatagencore "github.com/xataio/xata-go/xata/internal/fern-core/generated/go"
 	xatagenclient "github.com/xataio/xata-go/xata/internal/fern-core/generated/go/core"
@@ -13,10 +14,13 @@ type WorkspacesClient interface {
 	List(ctx context.Context) (*xatagencore.GetWorkspacesListResponse, error)
 	Create(ctx context.Context, request *WorkspaceMeta) (*xatagencore.Workspace, error)
 	Delete(ctx context.Context, workspaceID string) error
+	Get(ctx context.Context) (*xatagencore.Workspace, error)
+	GetWithWorkspaceID(ctx context.Context, workspaceID string) (*xatagencore.Workspace, error)
 }
 
 type workspaceCli struct {
-	generated xatagencore.WorkspacesClient
+	generated   xatagencore.WorkspacesClient
+	workspaceID string
 }
 
 func (w workspaceCli) List(ctx context.Context) (*xatagencore.GetWorkspacesListResponse, error) {
@@ -34,16 +38,33 @@ func (w workspaceCli) Delete(ctx context.Context, workspaceID string) error {
 	return w.generated.DeleteWorkspace(ctx, workspaceID)
 }
 
+func (w workspaceCli) Get(ctx context.Context) (*xatagencore.Workspace, error) {
+	return w.generated.GetWorkspace(ctx, w.workspaceID)
+}
+
+func (w workspaceCli) GetWithWorkspaceID(ctx context.Context, workspaceID string) (*xatagencore.Workspace, error) {
+	return w.generated.GetWorkspace(ctx, workspaceID)
+}
+
 func NewWorkspacesClient(opts ...ClientOption) (WorkspacesClient, error) {
 	cliOpts, err := consolidateClientOptionsForCore(opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	return workspaceCli{generated: xatagencore.NewWorkspacesClient(
-		func(options *xatagenclient.ClientOptions) {
-			options.HTTPClient = cliOpts.HTTPClient
-			options.BaseURL = cliOpts.BaseURL
-			options.Bearer = cliOpts.Bearer
-		})}, nil
+	dbCfg, err := loadDatabaseConfig()
+	if err != nil {
+		// No err, because the config values can be provided by the users.
+		log.Println(err)
+	}
+
+	return workspaceCli{
+		generated: xatagencore.NewWorkspacesClient(
+			func(options *xatagenclient.ClientOptions) {
+				options.HTTPClient = cliOpts.HTTPClient
+				options.BaseURL = cliOpts.BaseURL
+				options.Bearer = cliOpts.Bearer
+			}),
+		workspaceID: dbCfg.workspaceID,
+	}, nil
 }
