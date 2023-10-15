295fd4403b19c1d6afaf13c2f0e1ad4a5d0b4db3
omerdemirok <omerdemirok@icloud.com>
2023-10-15 11:50:22 +0100

    add integration tests for branch client

--- internal/integration-tests/setup_test_test.go
+++ internal/integration-tests/setup_test_test.go
@@ -1,17 +1,23 @@
 package integrationtests
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_setup_cleanup(t *testing.T) {
-	cfg, err := setup()
+	cfg, err := setupDatabase()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("unable to setup db: %v", err)
 	}
 	assert.NotNil(t, cfg)
+
+	err = setupTableWithColumns(context.Background(), cfg)
+	if err != nil {
+		t.Fatalf("unable to setup table: %v", err)
+	}
 
 	t.Logf("%#v", cfg)
 
