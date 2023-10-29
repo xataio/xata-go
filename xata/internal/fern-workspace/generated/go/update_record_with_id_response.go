3155a70921c0e805dee9682e718c69dcca0cc718
omerdemirok <omerdemirok@icloud.com>
2023-10-29 21:36:03 +0000

    update auto gen update record response type

--- xata/internal/fern-workspace/generated/go/update_record_with_id_response.go
+++ xata/internal/fern-workspace/generated/go/update_record_with_id_response.go
@@ -2,64 +2,67 @@
 
 package api
 
-import (
-	json "encoding/json"
-	fmt "fmt"
-)
+//import (
+//	json "encoding/json"
+//	fmt "fmt"
+//)
 
-type UpdateRecordWithIdResponse struct {
-	typeName                     string
-	Record                       *Record
-	UpdateRecordWithIdResponseId *UpdateRecordWithIdResponseId
-}
+type UpdateRecordWithIdResponse map[string]interface{}
 
-func NewUpdateRecordWithIdResponseFromRecord(value *Record) *UpdateRecordWithIdResponse {
-	return &UpdateRecordWithIdResponse{typeName: "record", Record: value}
-}
-
-func NewUpdateRecordWithIdResponseFromUpdateRecordWithIdResponseId(value *UpdateRecordWithIdResponseId) *UpdateRecordWithIdResponse {
-	return &UpdateRecordWithIdResponse{typeName: "updateRecordWithIdResponseId", UpdateRecordWithIdResponseId: value}
-}
-
-func (u *UpdateRecordWithIdResponse) UnmarshalJSON(data []byte) error {
-	valueRecord := new(Record)
-	if err := json.Unmarshal(data, &valueRecord); err == nil {
-		u.typeName = "record"
-		u.Record = valueRecord
-		return nil
-	}
-	valueUpdateRecordWithIdResponseId := new(UpdateRecordWithIdResponseId)
-	if err := json.Unmarshal(data, &valueUpdateRecordWithIdResponseId); err == nil {
-		u.typeName = "updateRecordWithIdResponseId"
-		u.UpdateRecordWithIdResponseId = valueUpdateRecordWithIdResponseId
-		return nil
-	}
-	return fmt.Errorf("%s cannot be deserialized as a %T", data, u)
-}
-
-func (u UpdateRecordWithIdResponse) MarshalJSON() ([]byte, error) {
-	switch u.typeName {
-	default:
-		return nil, fmt.Errorf("invalid type %s in %T", u.typeName, u)
-	case "record":
-		return json.Marshal(u.Record)
-	case "updateRecordWithIdResponseId":
-		return json.Marshal(u.UpdateRecordWithIdResponseId)
-	}
-}
-
-type UpdateRecordWithIdResponseVisitor interface {
-	VisitRecord(*Record) error
-	VisitUpdateRecordWithIdResponseId(*UpdateRecordWithIdResponseId) error
-}
-
-func (u *UpdateRecordWithIdResponse) Accept(v UpdateRecordWithIdResponseVisitor) error {
-	switch u.typeName {
-	default:
-		return fmt.Errorf("invalid type %s in %T", u.typeName, u)
-	case "record":
-		return v.VisitRecord(u.Record)
-	case "updateRecordWithIdResponseId":
-		return v.VisitUpdateRecordWithIdResponseId(u.UpdateRecordWithIdResponseId)
-	}
-}
+//
+//type UpdateRecordWithIdResponse struct {
+//	typeName                     string
+//	Record                       *Record
+//	UpdateRecordWithIdResponseId *UpdateRecordWithIdResponseId
+//}
+//
+//func NewUpdateRecordWithIdResponseFromRecord(value *Record) *UpdateRecordWithIdResponse {
+//	return &UpdateRecordWithIdResponse{typeName: "record", Record: value}
+//}
+//
+//func NewUpdateRecordWithIdResponseFromUpdateRecordWithIdResponseId(value *UpdateRecordWithIdResponseId) *UpdateRecordWithIdResponse {
+//	return &UpdateRecordWithIdResponse{typeName: "updateRecordWithIdResponseId", UpdateRecordWithIdResponseId: value}
+//}
+//
+//func (u *UpdateRecordWithIdResponse) UnmarshalJSON(data []byte) error {
+//	valueRecord := new(Record)
+//	if err := json.Unmarshal(data, &valueRecord); err == nil {
+//		u.typeName = "record"
+//		u.Record = valueRecord
+//		return nil
+//	}
+//	valueUpdateRecordWithIdResponseId := new(UpdateRecordWithIdResponseId)
+//	if err := json.Unmarshal(data, &valueUpdateRecordWithIdResponseId); err == nil {
+//		u.typeName = "updateRecordWithIdResponseId"
+//		u.UpdateRecordWithIdResponseId = valueUpdateRecordWithIdResponseId
+//		return nil
+//	}
+//	return fmt.Errorf("%s cannot be deserialized as a %T", data, u)
+//}
+//
+//func (u UpdateRecordWithIdResponse) MarshalJSON() ([]byte, error) {
+//	switch u.typeName {
+//	default:
+//		return nil, fmt.Errorf("invalid type %s in %T", u.typeName, u)
+//	case "record":
+//		return json.Marshal(u.Record)
+//	case "updateRecordWithIdResponseId":
+//		return json.Marshal(u.UpdateRecordWithIdResponseId)
+//	}
+//}
+//
+//type UpdateRecordWithIdResponseVisitor interface {
+//	VisitRecord(*Record) error
+//	VisitUpdateRecordWithIdResponseId(*UpdateRecordWithIdResponseId) error
+//}
+//
+//func (u *UpdateRecordWithIdResponse) Accept(v UpdateRecordWithIdResponseVisitor) error {
+//	switch u.typeName {
+//	default:
+//		return fmt.Errorf("invalid type %s in %T", u.typeName, u)
+//	case "record":
+//		return v.VisitRecord(u.Record)
+//	case "updateRecordWithIdResponseId":
+//		return v.VisitUpdateRecordWithIdResponseId(u.UpdateRecordWithIdResponseId)
+//	}
+//}
