5272a220e5a7b4d525f07f05bd083ee33486b8f5
Philip Krauss <philip@philipkrauss.at>
2023-11-29 09:51:29 +0100

    Add example for docs

---
+++ examples/list_workspaces.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/xataio/xata-go/xata"
+)
+
+func main() {
+	workspaces, err := xata.NewWorkspacesClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := workspaces.List(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v\n", *resp.Workspaces[0])
+}
